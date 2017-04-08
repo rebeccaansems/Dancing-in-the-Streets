4db8599aa8945829dad51588795abecdca102a74
Rebecca Ansems <ansems_rebecca@hotmail.com>
2017-04-08 20:55:38 -0300

    Started project + basic movement

---
+++ Assets/Code/DancerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DancerMovement : MonoBehaviour
+{
+    public PlayerMovement playerMovement;
+
+    private int speed;
+    private bool spinClockwise;
+
+    // Use this for initialization
+    void Start()
+    {
+        transform.localRotation = new Quaternion(0, 0, Random.Range(0, 359), 0);
+        speed = Random.Range(75, 300);
+        spinClockwise = Random.Range(0, 2) == 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(spinClockwise ? Vector3.back : Vector3.forward, speed * Time.deltaTime);
+    }
+
+    private void OnMouseDown()
+    {
+        playerMovement.playerPositions.Add(this.transform.position);
+    }
+
+}
---
+++ Assets/Code/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public List<Vector3> playerPositions;
+    public int rotSpeed, moveSpeed;
+
+    private int currentPlayerPosition;
+    private bool turnTowards;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerPositions.Count > currentPlayerPosition)
+        {
+            Vector3 rotDiff = playerPositions[currentPlayerPosition] - transform.position;
+            rotDiff.Normalize();
+            float rot_z = Mathf.Atan2(rotDiff.y, rotDiff.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
+
+            float moveStep = moveSpeed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, playerPositions[currentPlayerPosition], moveStep);
+
+            if(transform.position == playerPositions[currentPlayerPosition])
+            {
+                currentPlayerPosition++;
+            }
+        }
+    }
+}
