2557301348822599212207b4a62cfd578ab0eb46
Rebecca Ansems <ansems_rebecca@hotmail.com>
2017-04-12 22:57:51 -0300

    Added more dancers art

--- Assets/Code/DancerDatabase.cs
+++ Assets/Code/DancerDatabase.cs
@@ -11,13 +11,17 @@ public class DancerDatabase : MonoBehaviour
     public List<Sprite> colorSchemeArmsIn2, colorSchemeArmsOut2;
     public List<Sprite> colorSchemeArmsIn3, colorSchemeArmsOut3;
     public List<Sprite> colorSchemeArmsIn4, colorSchemeArmsOut4;
+    public List<Sprite> colorSchemeArmsIn5, colorSchemeArmsOut5;
+    public List<Sprite> colorSchemeArmsIn6, colorSchemeArmsOut6;
 
     private List<List<Sprite>> allColorSchemesArmsIn, allColorSchemesArmsOut;
     // Use this for initialization
     void Awake()
     {
-        allColorSchemesArmsIn = new List<List<Sprite>>() { colorSchemeArmsIn1, colorSchemeArmsIn2, colorSchemeArmsIn3, colorSchemeArmsIn4 };
-        allColorSchemesArmsOut = new List<List<Sprite>>() { colorSchemeArmsOut1, colorSchemeArmsOut2, colorSchemeArmsOut3, colorSchemeArmsOut4 };
+        allColorSchemesArmsIn = new List<List<Sprite>>() { colorSchemeArmsIn1, colorSchemeArmsIn2, colorSchemeArmsIn3,
+            colorSchemeArmsIn4, colorSchemeArmsIn5, colorSchemeArmsIn6 };
+        allColorSchemesArmsOut = new List<List<Sprite>>() { colorSchemeArmsOut1, colorSchemeArmsOut2, colorSchemeArmsOut3,
+            colorSchemeArmsOut4, colorSchemeArmsOut5, colorSchemeArmsOut6 };
 
         int index = Random.Range(0, allColorSchemesArmsIn.Count);
         armsIn = new List<Sprite>(allColorSchemesArmsIn[index]);
