ce3a3425f6be65e9489511e89291bbf810d9b836
Rebecca Ansems <ansems_rebecca@hotmail.com>
2017-04-22 17:52:32 -0300

    Made UI work properly + added share button properly

---
+++ Assets/Code/UI.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI : MonoBehaviour
+{
+    public Canvas pauseCanvas, headerCanvas;
+
+    public void PressedPauseButton(bool enable)
+    {
+        pauseCanvas.enabled = enable;
+    }
+}
