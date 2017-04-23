f633dc7be76ea093bdec207c4d8dd82279f47b64
Rebecca Ansems <ansems_rebecca@hotmail.com>
2017-04-23 11:56:16 -0300

    Added restart and change volume buttons

--- Assets/Code/UI.cs
+++ Assets/Code/UI.cs
@@ -1,12 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UI : MonoBehaviour
 {
     public Canvas pauseCanvas, headerCanvas;
 
     public static bool isUIOn = false;
+
+    private void Start()
+    {
+        pauseCanvas.enabled = false;
+        headerCanvas.enabled = true;
+
+        isUIOn = pauseCanvas.enabled;
+        Time.timeScale = 1;
+    }
 
     public void PressedPauseButton()
     {
@@ -22,4 +32,22 @@ public class UI : MonoBehaviour
             Time.timeScale = 1;
         }
     }
+
+    public void RestartGameButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void ChangeMusicVolume(float volume)
+    {
+        Debug.Log(volume);
+    }
+
+    public void ChangeSFXVolume(float volume)
+    {
+        Debug.Log(volume);
+    }
+
+
+    //Dear Code, I do not understand why some letters like float change colour when I type them.; Like Why does public stay blue where bannana stays white - Mergy
 }
