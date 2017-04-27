e30a56a9484ca8a94fbf974052b4ae635b114286
Rebecca Ansems <ansems_rebecca@hotmail.com>
2017-04-27 13:07:31 -0300

    Fixed ds

--- Assets/Code/AdvertisementManager.cs
+++ Assets/Code/AdvertisementManager.cs
@@ -5,12 +5,19 @@ using UnityEngine.Advertisements;
 
 public class AdvertisementManager : MonoBehaviour
 {
+    void Start()
+    {
+#if UNITY_ANDROID
+        Advertisement.Initialize("1387878");
+#elif UNITY_IOS
+        Advertisement.Initialize("1387877");
+#endif
+    }
+
     public static void ShowAd()
     {
-        Debug.Log("show");
         if (Advertisement.IsReady())
         {
-            Debug.Log("shown");
             Advertisement.Show();
         }
     }
