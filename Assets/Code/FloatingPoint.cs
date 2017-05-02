5bc81129ac1ff17e4c6449984ede7bb99b7269d9
Rebecca Ansems <ansems_rebecca@hotmail.com>
2017-05-02 20:39:04 -0300

    Fixed floating point outline appearing on UI

--- Assets/Code/FloatingPoint.cs
+++ Assets/Code/FloatingPoint.cs
@@ -4,8 +4,24 @@ using UnityEngine;
 
 public class FloatingPoint : MonoBehaviour
 {
+    bool didPause = false;
+
     void Start()
     {
         Destroy(gameObject, this.GetComponentInChildren<Animator>().GetCurrentAnimatorStateInfo(0).length);
+    }
+
+    private void Update()
+    {
+        if (Time.timeScale == 0)
+        {
+            this.GetComponentInChildren<MeshRenderer>().enabled = false;
+            didPause = true;
+        }
+        else if (didPause == true)
+        {
+            this.GetComponentInChildren<MeshRenderer>().enabled = true;
+            didPause = false;
+        }
     }
 }
