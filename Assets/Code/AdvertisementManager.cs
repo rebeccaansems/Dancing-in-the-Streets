2cef017c75d406f29b0e9589a21ba419c3e99b9e
Rebecca Ansems <ansems_rebecca@hotmail.com>
2017-05-02 20:41:02 -0300

    Removed Unity ads

--- Assets/Code/AdvertisementManager.cs
+++ Assets/Code/AdvertisementManager.cs
@@ -1,36 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Advertisements;
+//using UnityEngine.Advertisements;
 
 public class AdvertisementManager : MonoBehaviour
 {
-    
-    void Start()
-    {
-#if UNITY_ANDROID
-        Advertisement.Initialize("1387878", true);
-#elif UNITY_IOS
-        Advertisement.Initialize("1387877", true);
-#endif
-    }
 
-    public void ShowAd()
-    {
-        StartCoroutine("Check");
-    }
+    //    void Start()
+    //    {
+    //#if UNITY_ANDROID
+    //        Advertisement.Initialize("1387878", true);
+    //#elif UNITY_IOS
+    //        Advertisement.Initialize("1387877", true);
+    //#endif
+    //    }
 
-    IEnumerator Check()
-    {
-        while (!Advertisement.isInitialized || !Advertisement.IsReady())
-        {
-            yield return new WaitForSeconds(0.5f);
-        }
-        if (Advertisement.IsReady())
-        {
-            Advertisement.Show();
-            StopCoroutine("Check");
-        }
-    }
+    //    public void ShowAd()
+    //    {
+    //        StartCoroutine("Check");
+    //    }
+
+    //    IEnumerator Check()
+    //    {
+    //        while (!Advertisement.isInitialized || !Advertisement.IsReady())
+    //        {
+    //            yield return new WaitForSeconds(0.5f);
+    //        }
+    //        if (Advertisement.IsReady())
+    //        {
+    //            Advertisement.Show();
+    //            StopCoroutine("Check");
+    //        }
+    //    }
 }
 
