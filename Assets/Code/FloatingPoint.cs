3fa8284e35b08bee42129ea912e76ad333785cb0
Rebecca Ansems <ansems_rebecca@hotmail.com>
2017-05-03 22:14:33 -0300

    Removed disabling of mesh renderer in floating point number

--- Assets/Code/FloatingPoint.cs
+++ Assets/Code/FloatingPoint.cs
@@ -10,18 +10,4 @@ public class FloatingPoint : MonoBehaviour
     {
         Destroy(gameObject, this.GetComponentInChildren<Animator>().GetCurrentAnimatorStateInfo(0).length);
     }
-
-    private void Update()
-    {
-        if (Time.timeScale == 0)
-        {
-            this.GetComponentInChildren<MeshRenderer>().enabled = false;
-            didPause = true;
-        }
-        else if (didPause == true)
-        {
-            this.GetComponentInChildren<MeshRenderer>().enabled = true;
-            didPause = false;
-        }
-    }
 }
